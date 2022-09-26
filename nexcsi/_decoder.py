fb2bcc9062fa0bbc13163f79d42719d851561bdc
Aravind Reddy V <zerodividedby0@gmail.com>
2022-09-26 17:03:57 +0530

    Fix #1

--- nexcsi/_decoder.py
+++ nexcsi/_decoder.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 raspberrypi = ["raspberrypi", "rpi", "rpi4", "rpi3", "bcm43455c0", "bcm43455", "Raspberry Pi B3+/B4"]
 nexus5 = ["nexus5", "bcm4339", "Nexus 5"]
 nexus6p = ["nexus6p", "bcm4358", "Nexus 6P"]
@@ -5,16 +7,13 @@ rtac86u = ["rtac86u", "bcm4366c0", "Asus RT-AC86U"]
 
 
 def decoder(device):
-    if device in raspberrypi or nexus5:
+    if device in raspberrypi + nexus5:
         from nexcsi import interleaved
+        interleaved.unpack = partial(interleaved.unpack, device=device)
         return interleaved
-    elif device in nexus6p:
+    elif device in nexus6p + rtac86u:
         from nexcsi import floating
-        floating.unpack = lambda csi, device, fftshift: floating.unpack(csi, nexus6p[0], fftshift)
-        return floating
-    elif device in rtac86u:
-        from nexcsi import floating
-        floating.unpack = lambda csi, device, fftshift: floating.unpack(csi, rtac86u[0], fftshift)
+        floating.unpack = partial(floating.unpack, device=device)
         return floating
     else:
         raise ValueError(
